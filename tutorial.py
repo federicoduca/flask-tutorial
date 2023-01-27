368fea90bc707fb856d7e00a2decd7599ae09a5e
Federico Duca <f.duca.a@gmail.com>
2023-01-26 21:31:38 -0300

    update class 7

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+import sqlalchemy
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -17,26 +18,43 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+        flash("Login succesful :)")
         return redirect(url_for("user"))
     else:
+        if "user" in session:
+            flash("You are already logged in")
+            return redirect(url_for("user"))
+
         return render_template("login.html")
 
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
-        usr = session["user"]
-        return f"<h1>{usr}</h1>"
+        user = session["user"]
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+        else:
+            if "email" in session:
+                email = session["email"]
+            return render_template("user.html", email=email)
+        return redirect(url_for("login"))
+
     else:
+        flash("You are not logged in!", "info")
         return redirect(url_for("login"))
 
 
 @app.route("/logout")
 def logout():
     session.pop("user", None)
+    session.pop("email", None)
+    flash("You have been logged out!", "info")
     return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(debug=False)
+    app.run(debug=True)
+    # app.run(debug=False)
